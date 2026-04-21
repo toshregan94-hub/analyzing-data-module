3c28be4bad34c4bec0be777956f0cb0bd5632eb5
Regan Tosh <toshregan94@gmail.com>
2026-04-21 14:20:49 +0300

    New changes

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+const datejs = require('datejs');
 
+function combineUsers(...args){
+  const combineObject = {
+    users: []
+
+  };
+  for (const array of args){
+    combineObject.users = [...combinedObject.users, ...array];
+  }
+
+  combineObject.merge_date = new Date().toLocaleDateString('en-US',{
+    month: 'numeric',
+    day: 'numeric',
+    year:'numeric'
+  });
+  return combineObject;
+}
 
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+
